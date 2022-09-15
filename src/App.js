fdc6c68964b38a725c9c1331c7fa0bf4f6c45642
Scotty <sparlor91@gmail.com>
2022-09-15 16:29:18 -0400

    workflow

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -14,7 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer">
-          SCOTTY PARLOR RULES
+          Learn React
         </a>
       </header>
     </div>
